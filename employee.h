c71ea8a972ab5739a3249d7419fc4dd7a25ce057
Denis <anisiforov.deniska@mail.ru>
2022-10-07 14:51:22 +0700

    Добавлен employee.h

---
+++ employee.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "jobtitle.h"
+#include "equipment.h"
+using namespace std;
+
+class employee {
+public:
+	employee(int i, jobtitle j, equipment e);
+	employee(int x);
+	employee();
+	void output();
+	void input();
+	int empcost();
+private:
+	int id; //индивидуальный номер
+	jobtitle jt; //должность
+	equipment equ; //снар€жение
+};
