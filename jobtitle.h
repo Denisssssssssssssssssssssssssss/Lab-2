c125d194dbcbfd4d1b64233166aedddd6f8cdb76
Denis <anisiforov.deniska@mail.ru>
2022-10-07 14:59:53 +0700

    Добавлен jobtitle h

---
+++ jobtitle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class jobtitle
+{
+	friend class employee;
+public:
+	jobtitle(string j, int h);
+	jobtitle(int x);
+	jobtitle();
+	void input();
+	void output();
+private:
+	string jtitle; //наименование должности
+	int monthcost; //
+};
+
